b20631818afd92e71628df07b61a90a6c3fbbbdb
Jagannath Patil <neha.jagannath-patil@capgemini.com>
2021-10-16 11:20:41 +0530

    added new console application

---
+++ GitBashPracticeConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GitBashPracticeConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi");
+
+        }
+    }
+}
